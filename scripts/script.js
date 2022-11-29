845224ed3ceb782fc8a605c1c5a2aae0dc5da0e9
Asad Mushtaq <mmushtaq3@my.bcit.ca>
2022-11-28 19:53:55 -0800

    Added functionality for save button

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,10 @@ window.onload=function(){
     const cancel = document.querySelector('.cancel');
     const save = document.querySelector('.save');
 
+    const notesArray = [
+        {title: "note one", body: "This is my first note"},
+        {title: "none two", body: "This is my first note"} 
+    ];
 
     /* New Note Button Click Events */
     function newNoteBTN () {
@@ -16,31 +20,52 @@ window.onload=function(){
             textArea.value = '';
         }
         textArea.focus();
-    }
+    };
     newNote.addEventListener("click", newNoteBTN);
     
     /* Dark Theme Button Click Events */
     function darkThemeBTN () {
         if (darkTheme.textContent === 'Dark Theme') {
             darkTheme.textContent = 'Light Theme';
-            textArea.style.color = 'white'
+            textArea.style.color = 'white';
         } else {
             darkTheme.textContent = 'Dark Theme';
-            textArea.style.color = 'black'
+            textArea.style.color = 'black';
         }
         textArea.focus();
         document.querySelector('aside').classList.toggle('darkThemeBG1');
         document.querySelector('textarea').classList.toggle('darkThemeBG1');
         document.body.classList.toggle('textDarkTheme');
         document.body.classList.toggle('darkThemeBG2');
-    }
+    };
     darkTheme.addEventListener("click", darkThemeBTN);
+
+    /* Save Button Click Events */
+    function appendListItem (item) {
+        let listItem = document.createElement("li");
+        notesList = document.querySelector("aside ul");
+        notesList.appendChild(listItem)
+        listItem.textContent = noteTitle;
+    }
+
+    function saveBTN () {
+        do {
+            noteTitle = prompt("Please enter a title for this note: ");
+        } while (noteTitle.length < 1 || noteTitle.trim().length === 0);
+        noteBody = textArea.value;
+        let newNote = {title: noteTitle, body: noteBody};
+        notesArray.push(newNote);
+        textArea.value = '';
+        textArea.focus();
+        appendListItem(newNote);
+    };
+    save.addEventListener("click", saveBTN);
 
     /* Cancel Button Click Events */
     function cancelBTN () {
         save.style.display = 'none';
         cancel.style.display = 'none';
         textArea.style.display = 'none';
-    }
+    };
     cancel.addEventListener("click", cancelBTN);
 }
