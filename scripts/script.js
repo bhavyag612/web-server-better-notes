8f834ed604415c04007b21f2a3b46c32620adb9a
Asad Mushtaq <mmushtaq3@my.bcit.ca>
2022-11-28 18:17:30 -0800

    Added cancel and newNote button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
 window.onload=function(){
-    
+    const textArea = document.querySelector('textarea');
+    const newNote = document.querySelector('.newNote');
+    const darkTheme = document.querySelector('.darkTheme');
+    const cancel = document.querySelector('.cancel');
+    const save = document.querySelector('.save');
+
+    function cancelBTN() {
+        save.style.display = 'none';
+        cancel.style.display = 'none';
+        textArea.style.display = 'none';
+    }
+    cancel.addEventListener("click", cancelBTN)
+
+    function newNoteBTN () {
+        if (textArea.style.display === 'none') {
+            save.style.display = 'block';
+            cancel.style.display = 'block';
+            textArea.style.display = 'block';
+        } else {
+            textArea.value = '';
+        }
+        textArea.focus();
+    }
+    newNote.addEventListener("click", newNoteBTN)
 }
