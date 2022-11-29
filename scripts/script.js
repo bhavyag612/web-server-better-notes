b67b4f0494597241509c15e16cba1082aee24733
Asad Mushtaq <mmushtaq3@my.bcit.ca>
2022-11-28 19:03:33 -0800

    Added dark theme button functionality for text and background colors

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,8 @@ window.onload=function(){
     const cancel = document.querySelector('.cancel');
     const save = document.querySelector('.save');
 
-    function cancelBTN() {
-        save.style.display = 'none';
-        cancel.style.display = 'none';
-        textArea.style.display = 'none';
-    }
-    cancel.addEventListener("click", cancelBTN)
 
+    /* New Note Button Click Events */
     function newNoteBTN () {
         if (textArea.style.display === 'none') {
             save.style.display = 'block';
@@ -22,5 +17,30 @@ window.onload=function(){
         }
         textArea.focus();
     }
-    newNote.addEventListener("click", newNoteBTN)
+    newNote.addEventListener("click", newNoteBTN);
+    
+    /* Dark Theme Button Click Events */
+    function darkThemeBTN () {
+        if (darkTheme.textContent === 'Dark Theme') {
+            darkTheme.textContent = 'Light Theme';
+            textArea.style.color = 'white'
+        } else {
+            darkTheme.textContent = 'Dark Theme';
+            textArea.style.color = 'black'
+        }
+        textArea.focus();
+        document.querySelector('aside').classList.toggle('darkThemeBG1');
+        document.querySelector('textarea').classList.toggle('darkThemeBG1');
+        document.body.classList.toggle('textDarkTheme');
+        document.body.classList.toggle('darkThemeBG2');
+    }
+    darkTheme.addEventListener("click", darkThemeBTN);
+
+    /* Cancel Button Click Events */
+    function cancelBTN () {
+        save.style.display = 'none';
+        cancel.style.display = 'none';
+        textArea.style.display = 'none';
+    }
+    cancel.addEventListener("click", cancelBTN);
 }
